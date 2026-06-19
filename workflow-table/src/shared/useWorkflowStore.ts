b81f88ad84e214cd1685a83af6f610d0015ad592
DemnArctis <demnarctis@gmail.com>
2026-06-19 17:51:40 +0300

    Remove unused imports.

--- workflow-table/src/shared/useWorkflowStore.ts
+++ workflow-table/src/shared/useWorkflowStore.ts
@@ -1,6 +1,5 @@
 
-import { computed, readonly, ref, type DeepReadonly, type Ref } from 'vue';
-import { useObservable } from './useObservable';
+import { ref, type Ref } from 'vue';
 import { workflowStore, type WorkflowStore } from '@shared/workflow/store/WorkflowStore';
 import type { WorkflowStep } from '../../../root-config/src/shared/workflow/interfaces/WorkflowStep';
 import { defineStore } from 'pinia';
@@ -35,27 +34,8 @@ export const useWorkflowStore = defineStore<'workflow', AppWorkflowStore>('workf
 
   /** Функция для очистки подписок */
   const destroy = () => {
-    subscriptions.forEach(subscription => subscription.unsubscribe());
-  }
+    subscriptions.forEach(subscription => { subscription.unsubscribe(); });
+  };
 
   return { steps, name, createStep, changeStepName, removeStep, destroy };
-})
-
-/**
- * Пример реализации с помощью composable, если мы не хотим тащить store библиотеки (для минимизации бандла)
- * Только для демонстрации, не используется в приложении
- * Использует главный workflow store, для работы с состоянием рабочего процесса
- * @return состояние в виде реактивных свойст (Ref) и методы для его изменения
- */ /* @ts-ignore */
-function useWorkflowStoreWithutPinia(): AppWorkflowStore {
-  return {
-    // Состояние (state)
-    steps: useObservable(workflowStore.steps, []), // Можем использовать readonly() чтобы запретить менять состояние напрямую
-    name: useObservable(workflowStore.name, ''),
-    // Действия (аctions). Просто используем методы общего стора. Можно обернуть в функцию и добавить свою логику, если нужно.
-    createStep: workflowStore.createStep.bind(workflowStore), // Важно сделать bind, иначе методы не будут видеть внутренних свойств workflowStore, а будут искать их в этом объекте
-    changeStepName: workflowStore.changeStepName.bind(workflowStore),
-    removeStep: workflowStore.removeStep.bind(workflowStore),
-    destroy: () => { /* сейчас все отписки происходят автоматически, благодара useObservable */ }
-  };
-}
+});
