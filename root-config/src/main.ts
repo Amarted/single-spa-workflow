8be3cb71727d7c8b5fe1e0fd96438316801eb876
DemnArctis <demnarctis@gmail.com>
2026-06-20 19:11:10 +0300

    Use applicaiton loggr to log errors. Show message about server unavailability.

--- root-config/src/main.ts
+++ root-config/src/main.ts
@@ -3,6 +3,8 @@ import { workflowStore } from '@shared/workflow/store/WorkflowStore';
 import { messageService } from '@shared/MessageService';
 import './styles/styles.scss';
 import { workflowApiService } from './shared/workflow/api/WorkflowApiService';
+import { logger } from './shared/Logger';
+import { ApiError } from './shared/errors/ApiError';
 
 /** Данные микро-фронтенда */
 interface Frontend {
@@ -38,8 +40,9 @@ frontends.forEach((frontend) => {
 // Оповестить пользователя и сделать лог об ошибке загрузки приложения
 addErrorHandler((error) => {
   if (getAppStatus(error.appOrParcelName) === LOAD_ERROR) {
-    messageService.showToast(`Не удалось загрузить приложение ${error.appOrParcelName}`, 'error');
-    console.error(error);
+    const message = `Не удалось загрузить приложение ${error.appOrParcelName}`;
+    messageService.showToast(message, 'error');
+    logger.error(message, { error });
   }
 });
 
@@ -50,8 +53,16 @@ async function initState(): Promise<void> {
   try {
     await loadWorkflow();
   } catch (error) {
-    messageService.showToast('Не удалось загрузить данные с сервера. Возможно он не доступен', 'error');
-    console.error(error);
+    let errorMessage;
+    const serverUnavailable = (error instanceof ApiError && error.status === 503) || error instanceof Error && error.message === 'Failed to fetch';
+    if (serverUnavailable) {
+      errorMessage = 'Сервер недоступен. Попробуйте позже';
+    } else {
+      errorMessage = 'Не удалось загрузить данные с сервера. Попробуйте обновить страницу';
+    }
+    // Уведомить пользователя и залогировать ошибку
+    messageService.showToast(errorMessage, 'error', 'always');
+    logger.error(errorMessage, { error });
   }
 }
 
@@ -65,7 +76,8 @@ async function loadWorkflow(name?: string): Promise<void> {
     workflowStore.setWorkflow(result.value);
   } else {
     // Не валидные данные, может ошибка в имени - выдать оповещение и залогировать результат
-    messageService.showToast(`Ошибка загрузки рабочего процесса. ${result.error.message}`, 'error');
-    console.error('Ошибка загрузки рабочего процесса', result);
+    const errorMessage = 'Ошибка загрузки рабочего процесса';
+    messageService.showToast(`${errorMessage}: ${result.error.message}`, 'error');
+    logger.error(errorMessage, result);
   }
 }
